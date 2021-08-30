292fdc4d39cae8123ceeef5883bb6917a5a8813a
seanm <seanm@alleninstitute.org>
2021-08-30 16:08:37 -0700

    Fix initial view of audit info for annotations

--- src/renderer/containers/TemplateEditorModal/index.tsx
+++ src/renderer/containers/TemplateEditorModal/index.tsx
@@ -71,16 +71,23 @@ interface AnnotationKeys {
   value: string;
 }
 
+const AUDIT_ANNOTATION_KEYS = [
+  { key: "created", title: "Date Added to Template" },
+  { key: "createdByDisplayName", title: "Added to Template by" },
+  { key: "modified", title: "Last Modified" },
+  { key: "modifiedByDisplayName", title: "Last Modified by" },
+];
+const AUDIT_ANNOTATION_KEY_SET = new Set(
+  AUDIT_ANNOTATION_KEYS.map((k) => k.key)
+);
+
 const FOCUSED_ANNOTATION_KEYS = [
   { key: "name", title: "Name" },
   { key: "description", title: "Description" },
   { key: "annotationTypeName", title: "Data Type" },
   { key: "annotationOptions", title: "Dropdown Options" },
   { key: "lookupTable", title: "Lookup Reference" },
-  { key: "created", title: "Date Added to Template" },
-  { key: "createdByDisplayName", title: "Added to Template by" },
-  { key: "modified", title: "Last Modified" },
-  { key: "modifiedByDisplayName", title: "Last Modified by" },
+  ...AUDIT_ANNOTATION_KEYS,
 ];
 
 const FOCUSED_ANNOTATION_COLUMNS: ColumnProps<AnnotationKeys>[] = [
@@ -162,13 +169,20 @@ function TemplateEditorModal(props: Props) {
       const annotation = (focusedAnnotation || template.annotations[0]) as {
         [key: string]: any;
       };
-      const value = annotation[key] && castArray(annotation[key]).join(", ");
-      if (value) {
-        return [{ key: title, value }];
+      if (
+        !AUDIT_ANNOTATION_KEY_SET.has(key) ||
+        originalTemplate?.annotations.find(
+          (a) => a.annotationId === annotation?.annotationId
+        )
+      ) {
+        const value = annotation[key] && castArray(annotation[key]).join(", ");
+        if (value) {
+          return [{ key: title, value }];
+        }
       }
       return [];
     });
-  }, [focusedAnnotation, template.annotations]);
+  }, [focusedAnnotation, template.annotations, originalTemplate]);
 
   function onCloseAnnotationModal() {
     setAnnotationToEdit(undefined);
@@ -181,7 +195,6 @@ function TemplateEditorModal(props: Props) {
       annotationTypeName: annotation["annotationTypeId/Name"],
       required: false,
       orderIndex: template.annotations.length,
-      modified: undefined as any, // no-date is more true than any date here
     });
     dispatch(addExistingAnnotation(annotation));
   }
