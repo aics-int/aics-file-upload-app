85ea8a6112288f55489df78d56851c236eb7f319
Philip Garrison <philip.garrison@alleninstitute.org>
2022-11-14 13:10:43 -0800

    [SWE-550] Comment to explain 100MB limit

--- src/renderer/services/file-management-system/ChunkedFileReader.ts
+++ src/renderer/services/file-management-system/ChunkedFileReader.ts
@@ -5,7 +5,15 @@ import BatchedTaskQueue from "../../entities/BatchedTaskQueue";
 
 import Md5Hasher from "./Md5Hasher";
 
-const READ_STREAM_MAX_CHUNK_SIZE = 100 * 1000 * 1000; // 100 MB
+/* Limit the read stream to 100 MB chunks. This is required because with 300MB chunks the read
+ * stream allocates enough memory to hit Electron's maximum heap usage and crash the app.
+ * 100MB is big enough because our maximum chunk size for an upload should be 500MB, because S3
+ * supports files up to 5TB with up to 10,000 chunks, and 100MB here means at most five reads per
+ * chunk, which doesn't seem like much overhead.
+ * 100MB is small enough because on Philp's Macbook the app did not crash at 200MB; this gives us a
+ * 2x safety factor. Electron's maximum heap size should be consistent across machines.
+ */
+const READ_STREAM_MAX_CHUNK_SIZE = 100 * 1000 * 1000;
 
 // Create an explicit error class to capture cancellations
 export class CancellationError extends Error {
