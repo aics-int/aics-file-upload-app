a494cfe4828a0824985276ddd2ca961cb43cc0b4
chrishu <chrishu@alleninstitute.org>
2022-11-16 12:55:36 -0500

    SparkMD5 state serialize/deserialize, persistence

--- src/renderer/services/file-management-system/Md5Hasher.ts
+++ src/renderer/services/file-management-system/Md5Hasher.ts
@@ -2,22 +2,20 @@ import * as SparkMD5 from "spark-md5";
 
 export default class Md5Hasher{
 
-    /**
-     * Converts serialized string to a hasher object, having the same state as when it was serialized.
-     * @param serialized_md5 
-     * @returns 
-     */
-    public static deserialize (serialized_md5: string){
-        // const hasher = CryptoJS.algo.MD5.create();
-        
-        // Md5Hasher.deepCopy(JSON.parse(serialized_md5), hasher);
-        // return new Md5Hasher(hasher);        
-    }
-
     private hasher: SparkMD5.ArrayBuffer
 
     public constructor (hasher?: SparkMD5.ArrayBuffer) {
         this.hasher  = hasher || new SparkMD5.ArrayBuffer();
+    }
+
+
+    /**
+     * Converts serialized string to SparkMD5.State, having the same state as when it was serialized.
+     * @param serialized_md5 
+     * @returns 
+     */
+     public deserialize (serialized_md5: string){ 
+        this.hasher.setState(JSON.parse(serialized_md5));
     }
 
     /**
@@ -33,7 +31,7 @@ export default class Md5Hasher{
      * @returns Serialized Hasher string, which can be stored and used to resume hashing process.
      */
     public serialize():string {
-        return JSON.stringify(this.hasher);
+        return JSON.stringify(this.hasher.getState());
     }
 
     /** 
