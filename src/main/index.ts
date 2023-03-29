9bf844beadc0ffb6f44d30a98a3e1c78eecbcd46
Chris Hughes <hughes036@gmail.com>
2023-03-29 15:41:45 -0400

    Update src/main/index.ts
    Co-authored-by: Sean LeRoy <41307451+SeanLeRoy@users.noreply.github.com>

--- src/main/index.ts
+++ src/main/index.ts
@@ -96,7 +96,7 @@ function createMainWindow() {
 
 // This is a temporary fix for this issue: https://github.com/electron/electron/issues/23664
 app.commandLine.appendSwitch("disable-features", "OutOfBlinkCors");
-// This is to enable maula garbage collection SWE-784
+// This is to enable manual garbage collection SWE-784
 app.commandLine.appendSwitch('js-flags', '--expose-gc --trace-gc');
 
 // quit application when all windows are closed
