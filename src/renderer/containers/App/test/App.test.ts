a881ae1f7a5404b86c46f5151b2c41b978b75351
chughes <chrishu@alleninstitute.org>
2023-06-08 14:17:04 -0700

    testing App handleUploadJobUpdates

---
+++ src/renderer/containers/App/test/App.test.ts
@@ -0,0 +1,66 @@
+import { expect } from "chai";
+import { JSSJobStatus, Service } from "../../../services/job-status-service/types";
+import { JSSJob } from "../../../services/job-status-service/types";
+import { handleUploadJobUpdates } from "../index";
+import { Step } from "../../Table/CustomCells/StatusCell/Step";
+import { updateUploadProgressInfo } from "../../../state/job/actions";
+import { Action } from "redux-logic";
+
+describe("App", () => {
+    describe("handleUploadJobUpdates", () => {
+        [
+            {
+                serviceFields: {
+                    preUploadMd5: 5,
+                    fileSize: 10
+                }, 
+                step: Step.ONE, 
+                progressField: "preUploadMd5"
+            },
+            {
+                serviceFields: {
+                    preUploadMd5: 10,
+                    fileSize: 10,
+                    currentFileSize: 5,
+                }, 
+                step: Step.TWO,
+                progressField: "currentFileSize"
+            },
+            {
+                serviceFields: {
+                    preUploadMd5: 10,
+                    currentFileSize: 10,
+                    postUploadMd5: 5,
+                    fileSize: 10,
+                }, 
+                step: Step.THREE,
+                progressField: "postUploadMd5"
+            },
+        ].forEach(({serviceFields, step, progressField}) => {
+            it.only("dispatches updateUploadProgressInfo when pre-upload-md5 is in progress", () => {
+                // Arrange
+                const fssJob: JSSJob = {
+                    created: new Date(),
+                    jobId: "foo123",
+                    jobName: "test_file.txt",
+                    modified: new Date(),
+                    originationHost: "dev-aics-fup-001",
+                    service: Service.FILE_STORAGE_SERVICE,
+                    updateParent: false,
+                    user: "fakeuser",
+                    status: JSSJobStatus.WORKING,
+                    serviceFields,
+                };
+                let actionPersisted = undefined;
+                const dispatch = (action: Action)=>{
+                    actionPersisted = action;
+                }; 
+                const expectedAction = updateUploadProgressInfo(fssJob.jobId, { bytesUploaded: fssJob.serviceFields && fssJob.serviceFields[progressField], totalBytes: fssJob.serviceFields?.fileSize, step: step })
+                // Act
+                handleUploadJobUpdates(fssJob, dispatch);
+                // Assert
+                expect(actionPersisted).to.deep.equal(expectedAction);
+            });
+        });
+    });
+});
