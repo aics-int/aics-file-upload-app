4fcf9d2ac8596359584c84ba471615367ba8da23
tylerf <tylerf@alleninstitute.org>
2025-01-16 20:58:23 -0800

    [FUA-98] Create <DragAndDropPrompt /> component;

---
+++ src/renderer/containers/UploadSelectionPage/DragAndDropPrompt/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { OpenDialogOptions, ipcRenderer } from "electron";
+import { isEmpty } from "lodash";
+
+import { RendererProcessEvents } from '../../../../shared/constants';
+import { UploadType } from '../../../state/selection/types';
+
+const styles = require("./styles.pcss");
+
+interface DragAndDropPromptProps {
+    openDialogOptions: OpenDialogOptions;
+    onDrop: (files: string[]) => void;
+    uploadType: UploadType
+}
+
+export default function DragAndDropPrompt(props: DragAndDropPromptProps) {
+
+    const onBrowse = async () => {
+        const filePaths = await ipcRenderer.invoke(
+          RendererProcessEvents.SHOW_DIALOG,
+          props.openDialogOptions
+        );
+    
+        // If cancel is clicked, this callback gets called and filePaths is undefined
+        if (filePaths && !isEmpty(filePaths)) {
+          props.onDrop(filePaths);
+        }
+    };
+
+    let helpText: string | null= null;
+    if (props.uploadType) {
+      helpText = props.uploadType === UploadType.File
+        ? "Accepted file formats include: .csv, .jpeg, ..."
+        : "Accepted file formats include: .zarr, .sldy, ..."
+    }
+
+    return (
+        <div className={styles.content}>
+          <h2 className={styles.header}>Select file(s) to upload</h2>
+          <div className={styles.dropZone} onClick={onBrowse}>
+            <UploadOutlined className={styles.uploadIcon} />
+            <div>Drag&nbsp;and&nbsp;Drop</div>
+            <div className={styles.dropZoneHelpText}>{helpText}</div>
+          </div>
+          <Button className={styles.dropZoneBrowseButton} disabled={!props.openDialogOptions} onClick={onBrowse}>
+            Browse Files
+          </Button>
+        </div>
+    )
+}
