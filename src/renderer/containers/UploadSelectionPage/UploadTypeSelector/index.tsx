a9a9bb39ec9eb68bfcccaa2d868f0ce32970887a
tylerf <tylerf@alleninstitute.org>
2025-01-21 11:57:11 -0800

    [FUA-98] Style "Upload Type" radio button

--- src/renderer/containers/UploadSelectionPage/UploadTypeSelector/index.tsx
+++ src/renderer/containers/UploadSelectionPage/UploadTypeSelector/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { Radio, RadioChangeEvent } from 'antd';
+import { CopyOutlined, FileOutlined } from '@ant-design/icons';
 import { selectUploadType } from '../../../state/selection/actions';
 import { UploadType } from '../../../state/selection/types';
 import { getUploadType } from '../../../state/selection/selectors';
@@ -29,15 +30,31 @@ export default function UploadTypeSelector() {
             >
                 <Radio.Button
                     value={UploadType.File}
+                    className={styles.radioButton}
                     onChange={onChange}
-                >
-                    File
+                >  
+                    <div className={styles.radioButtonContent}>
+                        <FileOutlined className={styles.radioButtonIcon} />
+                        <div className={styles.radioButtonText}>
+                            <span className={styles.radioButtonTitle}>File</span>
+                            <br />
+                            <span className={styles.radioButtonHelpText}>A single file such as a .csv, .jpeg, etc.</span>
+                        </div>
+                    </div>
                 </Radio.Button>
                 <Radio.Button
                     value={UploadType.Multifile}
+                    className={styles.radioButton}
                     onChange={onChange}
                 >
-                    Multifile
+                    <div className={styles.radioButtonContent}>
+                        <CopyOutlined className={styles.radioButtonIcon} />
+                        <div className={styles.radioButtonText}>
+                            <span className={styles.radioButtonTitle}>Multifile</span>
+                            <br />
+                            <span className={styles.radioButtonHelpText}>Advanced file types such as .zarr, .sldy, etc.</span>
+                        </div>
+                    </div>
                 </Radio.Button>
             </Radio.Group>
         </div>
