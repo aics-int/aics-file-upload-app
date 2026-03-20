4ac8b4d9619ca3179e35ffbfc06c188f79200fc6
brian.kim <yrkim98@uw.edu>
2026-03-20 09:37:44 -0700

    add a loading wheel

--- src/renderer/containers/UploadSelectionPage/index.tsx
+++ src/renderer/containers/UploadSelectionPage/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip } from "antd";
+import { Button, Spin, Tooltip } from "antd";
 import { OpenDialogOptions } from "electron";
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -102,6 +102,7 @@ export default function UploadSelectionPage() {
               </Button>
             </span>
           </Tooltip>
+          {isMxsLoading && <Spin size="small" />}
         </PageFooter>
       </div>
     </div>
