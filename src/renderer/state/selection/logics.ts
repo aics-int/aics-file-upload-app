83b2f4e65c43d86c365911bf66a2e4a4f5a85a86
brian.kim <yrkim98@uw.edu>
2026-05-06 09:04:52 -0700

    change celldraglogic

--- src/renderer/state/selection/logics.ts
+++ src/renderer/state/selection/logics.ts
@@ -157,10 +157,18 @@ const stopCellDragLogic = createLogic({
   ) => {
     const { cellAtDragStart, rows } = ctx;
     if (cellAtDragStart && rows?.length) {
-      const rowIds = rows.map((row: UploadRowTableId) => row.id);
       const upload = getUpload(getState());
-      const value = upload[cellAtDragStart.rowId][cellAtDragStart.columnId];
-      dispatch(updateUploadRows(rowIds, { [cellAtDragStart.columnId]: value }));
+      const columnId = cellAtDragStart.columnId;
+      // get row ids that dont have autofill
+      const rowIds = rows
+        .map((row: UploadRowTableId) => row.id)
+        .filter((id: any) => !upload[id]?.autofilledFields?.includes(columnId));
+      const value = upload[cellAtDragStart.rowId][columnId];
+
+      // drag to copy protection against overwriting rows already autofilled by mxs
+      if (rowIds.length) {
+        dispatch(updateUploadRows(rowIds, { [columnId]: value }));
+      }
     }
     done();
   },
